5f05513af5e4b96ef9b950baaa3e30cba058946a
dimunyx <botnarudumitru10@gmail.com>
2026-06-27 12:00:00 +0300

    releases: 0.3.1

--- releases/0.3.1/main.cpp
+++ releases/0.3.1/main.cpp
@@ -122,7 +122,7 @@ int main(int argc, char* argv[]) {
         }
         if (arg == "-v" || arg == "--version") {
             std::cout << "dimfetch 0.3.1\n";
-	    std::cout << "Minimalistic fetch written in C++\n";
+	    std::cout << "Minimalistic fetch written in Cpp\n";
             return 0;
         }
     }
